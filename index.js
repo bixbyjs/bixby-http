c4fe961c3e4e5d1800e389f967a7f2a78210b74a
Jared Hanson <jaredhanson@gmail.com>
2017-03-01 08:14:51 -0800

    Remove service.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 exports = module.exports = {
   'authenticator': require('./xom/authenticator'),
-  'service': require('./xom/service'),
   'server': require('./xom/server'),
   'secureserver': require('./xom/secureserver')
 };
