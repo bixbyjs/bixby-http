9d1d8ab017c003682666c67b1ffe4b8dfde83591
Jared Hanson <jaredhanson@gmail.com>
2017-05-26 09:32:15 -0700

    Use new component terminology.

--- app/workflow/statestore.js
+++ app/workflow/statestore.js
@@ -4,8 +4,8 @@ exports = module.exports = function(container) {
   
   var factory = new Factory();
   
-  var createImplDecls = container.specs('http://i.bixbyjs.org/http/workflow/createStateStoreImpl');
-  return Promise.all(createImplDecls.map(function(spec) { return container.create(spec.id); } ))
+  var createImplComps = container.components('http://i.bixbyjs.org/http/workflow/createStateStoreImpl');
+  return Promise.all(createImplComps.map(function(comp) { return comp.create(); } ))
     .then(function(impls) {
       impls.forEach(function(impl) {
         factory.use(impl);
