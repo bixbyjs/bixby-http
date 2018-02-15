57206adee7009507e7d4718f2b94a8e6a63ae544
Jared Hanson <jaredhanson@gmail.com>
2018-02-14 20:16:02 -0800

    Clean up HTTP server.

--- app/server.js
+++ app/server.js
@@ -1,25 +1,21 @@
 exports = module.exports = function(container, settings, logger) {
   // Load modules.
-  var http = require('http');
-  
+  var http = require('http')
+    , normalizePort = require('../lib/utils').normalizePort;
   
   
   // TODO: Implement support for HTTP server cluster
   //       https://github.com/bixbyjs/bixby-express/blob/768baba27a9ad762ca648e27e12f0c366e3886d2/lib/boot/httpserver.js
   
-  var normalizePort = require('../lib/utils').normalizePort;
-  
-  var options = settings.get('http/server') || {};
   var server = http.createServer();
-  
-  
   server.once('listening', function() {
     var addr = this.address();
     logger.info('HTTP server listening on %s:%d', addr.address, addr.port);
   });
   
+  var options = settings.get('http/server') || {};
   var address = options.address;
-  var port = options.port !== undefined ? options.port : (normalizePort(process.env.PORT) || 8080);
+  var port = options.port !== undefined ? options.port : normalizePort(process.env.PORT || 8080);
 
   server.listen(port, address);
   
