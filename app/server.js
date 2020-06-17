c940ba6a288c52ec48403710659398b706373b5d
Jared Hanson <jaredhanson@gmail.com>
2020-06-16 18:26:35 -0700

    Remove dead code.

--- app/server.js
+++ app/server.js
@@ -1,41 +1,12 @@
-exports = module.exports = function(container, settings, logger) {
-  // Load modules.
-  var http = require('http')
-    , normalizePort = require('../lib/utils').normalizePort;
-  
+exports = module.exports = function() {
+  var http = require('http');
   
   // TODO: Implement support for HTTP server cluster
   //       https://github.com/bixbyjs/bixby-express/blob/768baba27a9ad762ca648e27e12f0c366e3886d2/lib/boot/httpserver.js
   
   var server = http.createServer();
   return server;
-  
-  
-  server.once('listening', function() {
-    var addr = this.address();
-    logger.info('HTTP server listening on %s:%d', addr.address, addr.port);
-  });
-  
-  // --http-server-address --http-server-port
-  // settings.get('http/server/address');
-  
-  console.log('SETTINGS:');
-  //console.log(settings.toObject());
-  //console.log(settings.get('http'));
-  
-  //var options = settings.get('http/server') || {};
-  var options = {};
-  var address = options.address;
-  var port = options.port !== undefined ? options.port : normalizePort(process.env.PORT || 8080);
-
-  server.listen(port, address);
-  
-  return server;
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/Server';
-exports['@require'] = [
-  '!container',
-  'http://i.bixbyjs.org/Settings',
-  'http://i.bixbyjs.org/Logger'
-];
+exports['@require'] = [];
