6443f8080ab55443e050f65bddcbe23f0b00110d
Jared Hanson <jaredhanson@gmail.com>
2020-07-24 08:45:43 -0700

    Consolidate verify into scheme.

--- app/auth/bearer/scheme.js
+++ app/auth/bearer/scheme.js
@@ -1,12 +1,34 @@
-exports = module.exports = function(verify) {
+exports = module.exports = function(tokens) {
   var Strategy = require('passport-http-bearer');
   
-  var strategy = new Strategy({ passReqToCallback: true }, verify);
+  var strategy = new Strategy({ passReqToCallback: true }, function(req, token, cb) {
+    
+    tokens.verify(token, function(err, claims) {
+      if (err) { return cb(err); }
+      
+      // TODO: Check confirmation methods, etc
+      
+      var info = {};
+      if (claims.scope) {
+        info.scope = claims.scope;
+      }
+      if (claims.client) {
+        info.client = claims.client;
+      }
+      //if (issuer) {
+      //  info.issuer = issuer;
+      //}
+      
+      // TODO: Support for authenticating the issuer, in self-issued scenarios without `sub`
+      return cb(null, claims.user, info);
+    });
+  });
+  
   return strategy;
 };
 
 exports['@implements'] = 'http://i.bixbyjs.org/http/auth/Scheme';
 exports['@scheme'] = 'bearer';
 exports['@require'] = [
-  './verify'
+  'http://i.bixbyjs.org/security/TokenService'
 ];
--- test/auth/bearer/scheme.test.js
+++ test/auth/bearer/scheme.test.js
@@ -19,22 +19,64 @@ describe('auth/bearer/scheme', function() {
     expect(factory['@singleton']).to.be.undefined;
   });
   
-  describe('creating scheme', function() {
+  describe('verifying token', function() {
+    var tokens = new Object();
+    tokens.verify = sinon.stub().yieldsAsync(null, {
+      client: {
+        id: 's6BhdRkqt3'
+      },
+      user: {
+        id: '248289761001'
+      }
+    });
+    
     var StrategySpy = sinon.spy(Strategy);
-    var verify = function(){};
     
     var factory = $require('../../../app/auth/bearer/scheme',
       { 'passport-http-bearer': StrategySpy });
-    var strategy = factory(verify);
+    var strategy = factory(tokens);
     
     it('should construct strategy', function() {
       expect(StrategySpy).to.have.been.calledOnce;
-      expect(StrategySpy).to.have.been.calledWithExactly({ passReqToCallback: true }, verify);
+      expect(StrategySpy).to.have.been.calledWith({ passReqToCallback: true });
     });
     
     it('should return strategy', function() {
       expect(strategy).to.be.an.instanceOf(Strategy);
     });
-  }); // creating scheme
+    
+    describe('verify', function() {
+      var user, info;
+      
+      before(function(done) {
+        var verify = StrategySpy.args[0][1];
+        verify({}, '2YotnFZFEjr1zCsicMWpAA', function(e, u, i) {
+          if (e) { return done(e); }
+          user = u;
+          info = i;
+          done();
+        });
+      });
+      
+      it('should verify token', function() {
+        expect(tokens.verify).to.calledOnceWith('2YotnFZFEjr1zCsicMWpAA');
+      });
+      
+      it('should yield user', function() {
+        expect(user).to.deep.equal({
+          id: '248289761001'
+        });
+      });
+      
+      it('should yield info', function() {
+        expect(info).to.deep.equal({
+          client: {
+            id: 's6BhdRkqt3'
+          }
+        });
+      });
+    }); // verify
+    
+  }); // verifying token
   
 });
