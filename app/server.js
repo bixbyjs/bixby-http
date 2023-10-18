6f11859565f2cd0dcc454b17cfd6d7c3d395b64e
Jared Hanson <jaredhanson@gmail.com>
2023-10-18 11:07:22 -0700

    Clean up.

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
+var http = require('http');
+
 exports = module.exports = function() {
-  var http = require('http');
-  
   // TODO: Implement support for HTTP server cluster
   //       https://github.com/bixbyjs/bixby-express/blob/768baba27a9ad762ca648e27e12f0c366e3886d2/lib/boot/httpserver.js
   
@@ -9,4 +9,3 @@ exports = module.exports = function() {
 };
 
 exports['@implements'] = 'module:http.Server';
-exports['@require'] = [];
